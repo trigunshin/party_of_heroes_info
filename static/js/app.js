c801f794e016157d9fd7e364a5e312072e55bfb1
pcrane <pcrane@goscoutgo.com>
2015-09-30 20:09:39 -0400

    Initial functionality; customizable hero levels, and upgrade tiers.

--- static/js/app.js
+++ static/js/app.js
@@ -1,224 +1,225 @@
-var CandidateAffix = React.createClass({
+// base_hero_stats
+// hero_levels
+// rarities
+
+var InfoAffix = React.createClass({
   render: function() {
     return (
       <div className='col-md-offset-4 col-md-4 affix' data-spy="affix">
-        <JobDetail current_job={this.props.current_job}/>
-        <CandidateForm current_job={this.props.current_job} />
+        
       </div>
     );
   }
 });
 
-var JobDetail = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <h4>{this.props.current_job.title}</h4>
-        <div className="panel panel-default">
-            <div className="panel-heading">
-              <h3 className="panel-title">Job Description</h3>
-            </div>
-          <div className="panel-body" style={{height: '200px', overflow: 'scroll'}}>
-            {this.props.current_job.description}
-          </div>
-        </div>
-      </div>
-    );
-  }
-});
-
-var CandidateForm = React.createClass({
+var HeroForm = React.createClass({
+  getDefaultProps: function() {
+    return {
+      heroes: base_hero_stats,
+      levels: hero_levels,
+      rarities: rarities
+    };
+  },
   handleSubmit: function(e) {
     e.preventDefault();
-    var job_id = React.findDOMNode(this.refs.job_id).value.trim();
-    var name = React.findDOMNode(this.refs.name).value.trim();
-    var email = React.findDOMNode(this.refs.email).value.trim();
+    var hero = React.findDOMNode(this.refs.hero_name).value.trim();
+    var rarity = React.findDOMNode(this.refs.rarity_type).value.trim();
+    var level = React.findDOMNode(this.refs.level_index).value.trim();
+    var ret = {'hero': hero, 'level': level, 'rarity': rarity};
 
-    var form_data = new FormData($('#candidate_submit_form')[0]);
-    form_data.append('candidate_name', name);
-    form_data.append('candidate_email', email);
-    form_data.append('job_id', job_id);
-
-    var fileInput = document.getElementById('resume_file');
-    var file = fileInput.files[0];
-    form_data.append('file', file);
-
-    $.ajax({
-        type: 'POST',
-        url: '/submit',
-        data: form_data,
-        contentType: false,
-        processData: false,
-        success: function(data) {
-            console.log('Success!');
-        },
-    });
-
+    this.props.onSubmitHero(ret);
     return;
   },
 
   render: function() {
+    // create option elements for each heroname
+    var hero_options = [];
+    _.each(_.values(this.props.heroes), function(hero) {
+      hero_options.push(<option value={hero.name}>{hero.name}</option>);
+    });
+    // create option elements for each level
+    var level_options = [];
+    _.each(_.values(this.props.levels), function(level) {
+      level_options.push(<option value={level.id}>{parseInt(level.Id, 10)+1}</option>);
+    });
+    // create option elements for each rarity
+    var rarity_options = []
+    _.each(_.values(this.props.rarities), function(rarity) {
+      rarity_options.push(<option value={rarity.name}>{rarity.name}</option>);
+    });
+
     return (
-      <form id='candidate_submit_form' name='candidate_submit_form' method="post" encType='multipart/form-data' onSubmit={this.handleSubmit}>
-        <input type="hidden" ref='job_id' className="form-control" id="candidate_name" placeholder="Name"
-          value={this.props.current_job.id} required></input>
+      <form id='hero_submit_form' className="form-inline" onSubmit={this.handleSubmit}>
         <div className="form-group">
-          <label htmlFor="candidate_name">Candidate Name</label>
-          <input type="text" ref='name' className="form-control" id="candidate_name" placeholder="Name" required></input>
+          <div className="input-group">
+            <select id='hero_name_selector' name='hero_name' ref='hero_name' className="form-control">
+              {hero_options}
+            </select>
+          </div>
+          <div className="input-group">
+            <select id='rarity_selector' name='rarity_type' ref='rarity_type' className="form-control">
+              {rarity_options}
+            </select>
+          </div>
+          <div className="input-group">
+            <select id='level_selector' name='level_index' ref='level_index' className="form-control">
+              {level_options}
+            </select>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="candidate_email">Candidate Email</label>
-          <input type="email" ref='email' className="form-control" id="candidate_email" placeholder="your@email.com" required></input>
-        </div>
-        <div className="form-group">
-          <label htmlFor="candidate_resume">Candidate Resume</label>
-          <input id='resume_file' type="file" ref='resume'   required ></input>
-          <p className="help-block">Upload the resume to use for this submission.</p>
-        </div>
-        <button type="submit" className="btn btn-default">
-          Submit
-        </button>
+        <button type="submit" className="btn btn-default">Add</button>
       </form>
     );
   }
 });
 
-var Job = React.createClass({
-  jobClicked: function(e) {
-    this.props.onJobClick(this.props.data);
+var Hero = React.createClass({
+  mixins: [React.addons.LinkedStateMixin],
+  _getImage: function(base_hero) {
+    if(base_hero.hero_type === 'Melee') return '/static/img/melee.png';
+    else if(base_hero.hero_type === 'Ranged') return '/static/img/ranged.png';
+    else return '/static/img/arcane.png';
+  },
+  getInitialState: function() {
+    return {
+      hero_name: this.props.heroInit.hero,
+      hero_level: this.props.heroInit.level,
+      hero_rarity: this.props.heroInit.rarity
+    };
+  },
+  get_stat: function(stat_multi, rarity_multi, base) {
+    // divide by 4096 due to raw number from game file
+    return Math.floor((stat_multi/4096) * base * (rarity_multi/4096));
   },
   render: function() {
+    var cur_level = hero_levels[parseInt(this.state.hero_level, 10) -1];
+    var base_hero = base_hero_stats[this.state.hero_name];
+
+    var stat_fn = _.partial(
+      this.get_stat,
+      cur_level.HealthMultiplier,
+      cur_level[rarities[this.state.hero_rarity].level_prop]);
+
+    // create option elements for each heroname
+    var hero_options = [];
+    _.each(_.values(base_hero_stats), function(hero) {
+      hero_options.push(<option value={hero.name}>{hero.name}</option>);
+    });
+    // create option elements for each level
+    var level_options = [];
+    _.each(_.values(hero_levels), function(level) {
+      level_options.push(<option value={level.id}>{parseInt(level.Id, 10)+1}</option>);
+    });
+    // create option elements for each rarity
+    var rarity_options = []
+    _.each(_.values(rarities), function(rarity) {
+      rarity_options.push(<option value={rarity.name}>{rarity.name}</option>);
+    });
+
     return (
-      <button data-job-id={this.props.data.id} type="button" className="list-group-item" onClick={this.jobClicked}>
-        <h4 className="list-group-item-heading">{this.props.data.title}</h4>
+      <li href="#" className="list-group-item">
+        <h4 className="list-group-item-heading">
+          L{this.state.hero_level} {this.state.hero_rarity} {this.state.hero_name}
+          <span className="label label-default">{base_hero.hero_type}</span>
+        </h4>
         <div className='row'>
-          <label className="col-md-2">Salary</label>
-          <div className="col-md-4">
-            <span>{this.props.data.salary_min}-{this.props.data.salary_max}</span>
+          <label className="col-md-2">Health</label>
+          <div className="col-md-2">
+            <span>{stat_fn(base_hero.health)}</span>
+          </div>
+          <label className="col-md-2">Damage</label>
+          <div className="col-md-2">
+            <span>{stat_fn(base_hero.dmg)}</span>
+          </div>
+          <label className="col-md-2">Armor</label>
+          <div className="col-md-2">
+            <span>{stat_fn(base_hero.armor)}</span>
           </div>
         </div>
         <div className='row'>
-          <label className="col-md-2">Fee</label>
-          <div className="col-md-4">
-            <span>{this.props.data.fee}%</span>
+          <label className="col-md-2">Active</label>
+          <div className="col-md-2">
+            <span>{cur_level.ActiveSkillLevel +1}</span>
+          </div>
+          <label className="col-md-2">Passive 1</label>
+          <div className="col-md-2">
+            <span>{cur_level.PassiveSkill1Level +1}</span>
+          </div>
+          <label className="col-md-2">Passive 2</label>
+          <div className="col-md-2">
+            <span>{cur_level.PassiveSkill2Level +1}</span>
           </div>
         </div>
-      </button>
+        <div className='row'>
+          <div className='col-md-2'>
+            <select id='hero_name_selector' valueLink={this.linkState('hero_name')} className="input-sm">
+              {hero_options}
+            </select>
+          </div>
+          <div className='col-md-2'>
+            <select id='rarity_selector' valueLink={this.linkState('hero_rarity')} className="input-sm">
+              {rarity_options}
+            </select>
+          </div>
+          <div className='col-md-2'>
+            <select id='level_selector' valueLink={this.linkState('hero_level')} className="input-sm">
+              {level_options}
+            </select>
+          </div>
+        </div>
+      </li>
     );
   }
 });
 
-var JobList = React.createClass({
-  onJobClicked: function(job) {
-    this.props.onJobClicked(job);
-  },
+var HeroList = React.createClass({
   render: function() {
-    var job_click = this.onJobClicked;
-    var jobNodes = this.props.data.map(function (job) {
+    var heroNodes = this.props.heroes.map(function (hero) {
       return (
-        <Job data={job} onJobClick={job_click} />
+        <Hero heroInit={hero} />
       );
     });
     return (
-      <div className="list-group">
-        {jobNodes}
+      <ul className="list-group">
+        {heroNodes}
+      </ul>
+    );
+  }
+});
+
+var HeroBox = React.createClass({
+  componentDidMount: function() {},
+  render: function() {
+    return (
+      <div className="heroBox col-md-8">
+        <HeroList heroes={this.props.heroes} />
       </div>
     );
   }
 });
 
-var JobForm = React.createClass({
-  handleSubmit: function(e) {
-      e.preventDefault();
-      var text = React.findDOMNode(this.refs.req_search).value.trim();
-      console.info('search for', text);
-      this.props.onJobSearchSubmit({title: text});
-      return;
-  },
-  render: function() {
-    return (
-      <form className="form-inline" onSubmit={this.handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="req_search">Job Search</label>
-          <input type="text" className="form-control" ref="req_search" id="req_search"
-            placeholder="Software Engineer"></input>
-        </div>
-        <button type="submit" className="btn btn-default">Search</button>
-      </form>
-    );
-  }
-});
-
-var JobBox = React.createClass({
+var HeroPane = React.createClass({
   getInitialState: function() {
-    return {data: [{
-        title:'Select a Job',
-        description: 'Description will show up here'
-    }]};
+    return {heroes: [ {hero: "Abomination", level: "1", rarity: "Common"}, {hero: "Abomination", level: "1", rarity: "Common"}]};
   },
-  componentDidMount: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        //console.log(data);
-        this.setState({data: data.data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  handleJobClick: function(job) {
-    this.props.onJobClicked(job);
-  },
-  handleJobSearchSubmit: function(data) {
-    // TODO: submit to the server and refresh the list
-    console.info('searching submit', data);
-    $.ajax({
-      url: this.props.url,
-      data: data,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data.data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
+  handleHeroSubmit: function(hero) {
+    var new_heroes = this.state.heroes;
+    console.info(hero);
+    new_heroes.push(hero);
+    this.setState({heroes: new_heroes});
   },
   render: function() {
     return (
-      <div className="jobBox col-md-6">
-        <JobForm onJobSearchSubmit={this.handleJobSearchSubmit} />
-        <JobList data={this.state.data} onJobClicked={this.handleJobClick}/>
+      <div>
+        <InfoAffix />
+        <HeroForm onSubmitHero={this.handleHeroSubmit} />
+        <HeroBox heroes={this.state.heroes} />
       </div>
     );
   }
 });
 
-var SearchPane = React.createClass({
-  getInitialState: function() {
-    return {current_job: {
-        title:'Select a Job',
-        description: 'Description will show up here'
-    }};
-  },
-  handleJobClick: function(job) {
-    this.setState({current_job: job});
-  },
-  render: function() {
-    return (
-      <div role="tabpanel" className="tab-pane active" id="search">
-        <CandidateAffix current_job={this.state.current_job} />
-        <JobBox url="/search" onJobClicked={this.handleJobClick} />
-      </div>
-    );
-  }
-});
 React.render(
-  <SearchPane />,
+  <HeroPane />,
   document.getElementById('tab_content_anchor')
 );
